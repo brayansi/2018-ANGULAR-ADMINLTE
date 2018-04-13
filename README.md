2b9cdd3a8db14764521cfd465d34dd2e2c5bae93
unknown <aluno0408@LABF4026195.fuvs.br>
2018-04-13 20:40:10 -0300

    Integração angular 5 e admin lte

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 2018-angular5-adminlte2.4
+
+
+https://pt.linkedin.com/pulse/criando-um-projeto-angular-4-com-template-adminlte-soares-bentes
--- crudProduct/src/app/app.module.ts
+++ crudProduct/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SettingsComponent } from './settings/settings.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SettingsComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
